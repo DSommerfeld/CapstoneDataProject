915ccdf42ef038aff56acbe41c08e70ef71f27ed
DSommerfeld <162390500+DSommerfeld@users.noreply.github.com>
2025-12-05 20:42:19 -0800

    Update visuals.py

--- visuals.py
+++ visuals.py
@@ -63,6 +63,7 @@ def plot_day_of_week(df):
 def plot_monthly_trend(df):
     """Line graph for showcasing number of crashes for each month"""
     st.subheader("Monthly Collision Trends for 2020 - 2025")
+    st.write("DEBUGGING MONTHS:", df['MONTHNAME'].unique()) 
     # List for shortened month names
     month_order = [
         "Jan", "Feb", "Mar", "Apr", "May", "Jun",
@@ -197,6 +198,7 @@ def plot_dangerous_streets(df):
     st.pyplot(fig)
 
     st.write(streetcounts.to_frame("Crash Count"))
+
 
 
 
